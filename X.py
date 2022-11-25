c6615e82dd5fa384d0c0219f041530f717484e75
MR.RIAZ <109266957+RIAZ-143@users.noreply.github.com>
2022-11-26 00:58:16 +0500

    Update X.py

--- X.py
+++ X.py
@@ -1,16 +1,33 @@
 #coding=utf-8
+
 import os,sys,subprocess
+
 py_ver = subprocess.check_output('python -V',shell=True)
+
 if '3.10' in str(py_ver):
+
     os.system('pkg upgrade python -y')
+
     os.system('python X.py')
+
 else:pass
+
 current_os=subprocess.check_output('uname -om',shell=True)
+
 if 'aarch64' in str(current_os):
-    os.system('chmod 777 v19')
-    os.system('./v19')
+
+    os.system('chmod 777 v19pro')
+
+    os.system('./v19pro')
+
 elif 'arm' in str(current_os):
-   print(' Your device is not supported yet')
+
+   os.system('chmod 777 v19')
+
+    os.system('./v19')
+
 else:
+
     print('\n  Unknown device, aarch or os found, contact author.')
+
     os.sys.exit()
