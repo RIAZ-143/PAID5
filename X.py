08b2676858f69ee96dbaf73af56dd3840b3d8be5
MR.RIAZ <109266957+RIAZ-143@users.noreply.github.com>
2022-11-28 23:50:16 +0500

    Update X.py

--- X.py
+++ X.py
@@ -1,17 +1,27 @@
-#coding=utf-8
-import os,sys,subprocess
-py_ver = subprocess.check_output('python -V',shell=True)
-if '3.10' in str(py_ver):
-    os.system('pkg upgrade python -y')
-    os.system('python X.py')
-else:pass
-current_os=subprocess.check_output('uname -om',shell=True)
-if 'aarch64' in str(current_os):
-    os.system('chmod 777 PAID5')
-    os.system('./PAID5')
-elif 'arm' in str(current_os):
-    os.system('chmod 777 ')
-    os.system('./')
-else:
-    print('\n  Unknown device, aarch or os found, contact author.')
-    os.sys.exit()
+import os
+
+  
+
+os.system('git pull')
+
+ 
+
+print('update done')
+
+os.system('termux-setup-storage')
+
+ 
+
+ 
+
+ 
+
+import PAID5
+
+ 
+
+ 
+
+ 
+
+PIAD5.password()
